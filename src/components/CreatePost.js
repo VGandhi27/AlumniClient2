9deac7ed7642c888d5042fb653b6b921373c7d56
Vidushi Gandhi <curiousvidushi27@gmail.com>
2023-11-09 20:31:03 +0000

    working on post

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,6 +1,61 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import M from 'materialize-css'
+import {useNavigate} from 'react-router-dom'
 import Img2 from '../assets/Alumni Students 0 (2).png'
 const CreatePost = () => {
+    let navigate =useNavigate();
+  const [title,setTitle] = useState("")
+    const [body,setBody] = useState("")
+    const [image,setImage] = useState("")
+    const [url,setUrl] = useState("")
+    useEffect(()=>{
+       if(url){
+        fetch("/createpost",{
+            method:"post",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem("jwt")
+            },
+            body:JSON.stringify({
+                title,
+                body,
+                pic:url
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+        
+           if(data.error){
+              M.toast({html: data.error,classes:"#c62828 red darken-3"})
+           }
+           else{
+               M.toast({html:"Created post Successfully",classes:"#43a047 green darken-1"})
+               navigate('/')
+           }
+        }).catch(err=>{
+            console.log(err)
+        })
+    }
+    },[url])
+  
+   const postDetails = ()=>{
+       const data = new FormData()
+       data.append("file",image)
+       data.append("upload_preset","insta_clone")
+       data.append("cloud_name","dcyysbj41")
+       fetch("https://api.cloudinary.com/v1_1/dcyysbj41/image/upload",{
+           method:"post",
+           body:data
+       })
+       .then(res=>res.json())
+       .then(data=>{
+          setUrl(data.url)
+       })
+       .catch(err=>{
+           console.log(err)
+       })
+
+    
+   }
   return (
     <div className="card input-filed"
        style={{
@@ -13,20 +68,20 @@ const CreatePost = () => {
            <input 
            type="text"
             placeholder="title"
-            // value={title}
-            // onChange={(e)=>setTitle(e.target.value)}
+            value={title}
+            onChange={(e)=>setTitle(e.target.value)}
             />
            <input
             type="text"
              placeholder="body"
-            //  value={body}
-            // onChange={(e)=>setBody(e.target.value)}
+             value={body}
+            onChange={(e)=>setBody(e.target.value)}
              />
            <div className="file-field input-field">
             <div className="btn #64b5f6 blue darken-1">
                 <span>Upload Image</span>
                 <input type="file" multiple
-                // onChange={(e)=>setImage(e.target.files[0])} 
+                onChange={(e)=>setImage(e.target.files[0])} 
                 />
             </div>
             <div className="file-path-wrapper">
@@ -34,7 +89,7 @@ const CreatePost = () => {
             </div>
             </div>
             <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
-            // onClick={()=>postDetails()}
+            onClick={()=>postDetails()}
             
             >
                 Submit post
